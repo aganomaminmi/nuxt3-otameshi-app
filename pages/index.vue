d5e7e9ae50b9b01992fba312d6ef47edbfce7ea0
Kohta Takanami <ripplekouha@gmail.com>
2022-05-18 18:37:39 +0900

    [init] first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="height: 2000px">
+    <h1>hello world!</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+onMounted(() => {
+  if (process.client) {
+    // window.addEventListener('scroll', () => {}, { passive: true })
+    window.addEventListener('scroll', (e) => {
+      // console.log(e.preventDefault())
+      
+      console.log('aaa', new Date().toString())
+    }, { passive: true })
+  }
+
+})
+</script>
